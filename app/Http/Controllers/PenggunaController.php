c0b17f524b902d5492da5a855079af5953558d2f
RuQzyy <Muhammadfaruq096@gmail.com>
2025-11-09 14:54:40 +0900

    masukan backend pada event dan terjemahan.

--- app/Http/Controllers/PenggunaController.php
+++ app/Http/Controllers/PenggunaController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Event;
+use Carbon\Carbon;
+use App\Models\Bahasa;
+
 
 class PenggunaController extends Controller
 {
     // Halaman utama (about)
     public function quiz()
     {
-        return view('pengguna.quiz');
+          $bahasa = Bahasa::all();
+         return view('pengguna.quiz', compact('bahasa'));
     }
 
     // Halaman budaya
@@ -25,10 +30,16 @@ class PenggunaController extends Controller
     }
 
     // Halaman index (misalnya home)
-    public function index()
-    {
-        return view('pengguna.index');
-    }
+      public function index()
+{
+    $currentYear = Carbon::now()->year; // Tahun sekarang (misalnya 2025)
+
+    $events = Event::whereYear('tanggal', $currentYear)
+        ->latest()
+        ->get();
+
+    return view('pengguna.index', compact('events'));
+}
 
     // Dashboard (hanya untuk pengguna login)
     public function dashboard()
